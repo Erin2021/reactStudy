296815dc3a5a8c25c142987b09ec9b7be215662d
Erin2021 <appleyummy78@gmail.com>
2024-08-02 18:04:37 +0900

    ch05

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -5,11 +5,13 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 import Library from './chapter_03/Library';
+import Clock from './chapter_04/Clock';
+import CommentList from './chapter_05/CommentList';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Library />
+    <CommentList />
   </React.StrictMode>
 );
 
